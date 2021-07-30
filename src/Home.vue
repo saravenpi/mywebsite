0cada434cda3c81fc0f8f5e3ac12988906d4d223
saravenpi <saravenpi@protonmail.com>
2021-07-30 16:53:31 +0200

    sketchy update

--- src/Home.vue
+++ src/Home.vue
@@ -1,8 +1,74 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+
+  <head>
+
+      <meta charset="utf-8">
+
+      <meta name="viewport" content="width=device-width, initial-scale=1">
+
+      <meta name="title" content="Saravenpi">
+
+      <meta name="description" content="Debugging Humanity">
+
+      <meta name="theme-color" content="#C0FDFF">
+
+      <meta property="og:image" content="https://avatars1.githubusercontent.com/u/61117321">
+
+      <meta name="author" content="saravenpi">
+
+      <meta name="keywords" content="saravenpi">
+
+      <meta name="color" content="#C0FDFF">
+
+      <title>Saravenpi</title>
+
+      <link rel="icon" href="https://avatars1.githubusercontent.com/u/61117321">
+
+      <link rel="stylesheet" href="style.css">
+
+      <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+
+
+  </head>
+
+
+      <div class="container">
+        <div class="topnav" id="myTopnav">
+          <a href="/" class="home">Home</a>
+          <a href="/skills">My Skills</a>
+          <a href="/projects">My Projects</a>
+          <a href="javascript:void(0);" class="icon" onclick="myFunction()">
+            <i class="material-icons">menu</i>
+          </a>
+        </div>
+
+          <div class="content">
+            <div id="music-status">
+
+            </div>
+              <h1>
+                <div class="profile">
+                  <img src="https://avatars.githubusercontent.com/u/61117321" class="avatar"> <div class="username"> Saravenpi</div>
+                </div>
+              </h1>
+
+              <p>
+                  Hello, my name is Sara and I'm a fullstack web developer! I'm constantly learning new languages and frameworks!
+
+                  <h1>Contact:</h1>
+                    Get in touch with me on Twitter/Telegram/Github <a href="https://twitter.com/saravenpi"><code>@saravenpi</code></a>
+
+                  <h1>Donate:</h1>
+                    <script type="text/javascript" src="https://cdnjs.buymeacoffee.com/1.0.0/button.prod.min.js" data-name="bmc-button" data-slug="saravenpi" data-color="#C0FDFF" data-emoji="" data-font="Poppins" data-text="Buy me a coffee" data-outline-color="#000000" data-font-color="#000000" data-coffee-color="#FFDD00" ></script>
+              </p>
+          </div>
+
+      </div>
+
+
+      <script type="text/javascript" src="./script.js"></script>
+
+
 </template>
 
 <script>
@@ -12,17 +78,15 @@ export default {
   name: 'app',
   components: {
     HelloWorld
-  }
+  },
+  mounted
 }
 </script>
 
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+<script type="text/javascript" src=".assets/script.js">
+
+</script>
+
+<style src="./assets/style.css">
+
 </style>
