9c587676e02f3af43cc4112486b9fe883347c959
saravenpi <saravenpi@protonmail.com>
2021-07-30 16:40:13 +0200

    fjdsklf;jsd

--- src/main.js
+++ src/main.js
@@ -3,6 +3,26 @@ import App from './App.vue'
 
 Vue.config.productionTip = false
 
+
+
+const NotFound = { template: '<p>Page not found</p>' }
+
+const About = { template: '<p>about page</p>' }
+
+const routes = {
+  '/': App,
+  '/about': About
+}
+
 new Vue({
-  render: h => h(App),
-}).$mount('#app')
+  el: '#app',
+  data: {
+    currentRoute: window.location.pathname
+  },
+  computed: {
+    ViewComponent () {
+      return routes[this.currentRoute] || NotFound
+    }
+  },
+  render (h) { return h(this.ViewComponent) }
+})
