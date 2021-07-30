4e7e2d591e18833c5400a5301059d5ac9c1488c8
saravenpi <saravenpi@protonmail.com>
2021-07-30 18:33:50 +0200

    vue router

--- src/main.js
+++ src/main.js
@@ -1,29 +1,23 @@
 import Vue from 'vue'
+import VueRouter from 'vue-router'
+Vue.config.productionTip = false
+Vue.use(VueRouter)
+
+const NotFound = { template: '<p>Page not found</p>' }
 import Home from './Home.vue'
 import Skills from './Skills.vue'
 import Projects from './Projects.vue'
-Vue.config.productionTip = false
 
+const routes = [
+  { path: '/', component: Home },
+  { path: '/skills', component: Skills },
+  { path: '/projects', component: Projects }
+]
 
-
-const NotFound = { template: '<p>Page not found</p>' }
-
-
-const routes = {
-  '/': Home,
-  '/skills': Skills,
-  '/projects': Projects
-}
-
-new Vue({
-  el: '#app',
-  data: {
-    currentRoute: window.location.pathname
-  },
-  computed: {
-    ViewComponent () {
-      return routes[this.currentRoute] || NotFound
-    }
-  },
-  render (h) { return h(this.ViewComponent) }
+const router = new VueRouter({
+  routes
 })
+
+const app = new Vue({
+  router
+}).$mount('#app')
